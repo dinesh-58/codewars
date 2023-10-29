d5ef459e5a49564ff753f148693cce11ba0325ba
dinesh-58 <86787475+dinesh-58@users.noreply.github.com>
2023-10-29 08:20:18 +0545

    modify comments for today's solution
    Idea of setting nums[i] = 1 temporarily performed worse at 67ms

--- neetcode150/arrays-and-hashing/product-array-except-self.js
+++ neetcode150/arrays-and-hashing/product-array-except-self.js
@@ -1,8 +1,11 @@
+/* test results:
+    * Case 1: 62ms
+    * Case 2: 62ms
+*/
+
 var productExceptSelf = function(nums) {
     const product = [];
     for(i in nums) {
-        // test performance results when temporarily setting nums[i] = 1 instead of checking condition below
-        // 2nd idea: just use nested for loop and continue; if i == j
         product.push(nums.reduce((acc, x, j) => 
             i == j ? acc  : acc * x 
         , 1));
