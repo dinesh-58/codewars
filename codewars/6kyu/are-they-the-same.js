adeed98b7789a49e23857ba66c2af6d0ced5a14c
dinesh-58 <86787475+dinesh-58@users.noreply.github.com>
2023-11-05 07:53:06 +0545

    implement fix for invalid arguements

--- codewars/6kyu/are-they-the-same.js
+++ codewars/6kyu/are-they-the-same.js
@@ -1,10 +1,13 @@
 function comp(array1, array2) {
+    if(! (Array.isArray(array1) && Array.isArray(array2))) return false;
+    if(array1.length !== array2.length) return false;
+
     const sameLastIndex ={};
     /* 
         * this object stores { array1num : index at which its same(square) was last found }
         * includes() searches from that index onwards
         */
-    return array1.every((a, i) => {
+    return array1.every(a => {
         // a^2 must occur same number of times as a
         const searchIndex = sameLastIndex[a] + 1 || 0;
         if (array2.includes(a*a, searchIndex)) {
