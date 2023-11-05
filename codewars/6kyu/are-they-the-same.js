b5e08ef74a24cfdc5c714e3d716ef0f1f7757dac
dinesh-58 <86787475+dinesh-58@users.noreply.github.com>
2023-11-05 07:42:13 +0545

    fix faulty code for storing sameLastIndex value
    idk if this passes remaining test. internet went out

--- codewars/6kyu/are-they-the-same.js
+++ codewars/6kyu/are-they-the-same.js
@@ -1,6 +1,19 @@
 function comp(array1, array2) {
-    array1.every(a => {
+    const sameLastIndex ={};
+    /* 
+        * this object stores { array1num : index at which its same(square) was last found }
+        * includes() searches from that index onwards
+        */
+    return array1.every((a, i) => {
         // a^2 must occur same number of times as a
-        return array2.includes(a*a);
+        const searchIndex = sameLastIndex[a] + 1 || 0;
+        if (array2.includes(a*a, searchIndex)) {
+            sameLastIndex[a] = array2.indexOf(a*a, searchIndex)
+            return true; 
+        }
+        else return false;
     })
+
+
+    // idea: sort both arrays and check index by index if arr[b] == arr[a] ** 2?
 }
