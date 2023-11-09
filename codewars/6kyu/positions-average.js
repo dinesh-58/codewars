a52aa52a629e51156dbd08866953ea01348420af
dinesh-58 <86787475+dinesh-58@users.noreply.github.com>
2023-11-09 09:58:26 +0545

    add comment for how to fix today's solution

--- codewars/6kyu/positions-average.js
+++ codewars/6kyu/positions-average.js
@@ -1,25 +1,14 @@
-/*
-"466960, 069060, 494940, 060069, 060090, 640009, 496464, 606900, 004000, 944096"
-    =>  26.6666666667)
-"444996, 699990, 666690, 096904, 600644, 640646, 606469, 409694, 666094, 606490"
-    => 29.2592592593)
-*/
-    
-// const test = "466960, 069060, 494940, 060069, 060090, 640009, 496464, 606900, 004000, 944096"
-const test = "444996, 699990, 666690, 096904, 600644, 640646, 606469, 409694, 666094, 606490";
-// const test = "6900690040, 4690606946, 9990494604"
-console.log(posAverage(test))
-// NOTE: comparison is being repeated. i.e. s[1] is compared w/ s[2] then s[2] is being compared with s[1]
-
 function posAverage(s) {
     s = s.split(', ')
     console.log(s);
     let commonPosCount = 0; 
-    const totalPositions = s.length * s[0].length;  
-    // all strings in testcases will have same length so we can just multiply (no. of strings) by (length of any string)
+    /* NOTE: to get correct answer, set totalPositions = C(s.length, 2) * s[0].length instead
+     * where C represents combinations. something like math.js implements math.combinations()
+    */
+    //const totalPositions = s.length * s[0].length;  
+    
     // do dumb way for now. comparing each string and character
     s.forEach((str, i) => {
-        // need another loop here for comparing with all otgher strings
         for(let j = i+1; j < s.length; j++) {
             for(const k in str) {
                 if(str[k] === s[j][k]) commonPosCount++;
@@ -30,3 +19,4 @@ function posAverage(s) {
     console.table({commonPosCount, totalPositions})
     return ((commonPosCount / totalPositions) * 100).toFixed(10);
 }
+
