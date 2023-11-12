c4111f60c78387c46ed524ad3c75cbd6bee48a06
dinesh-58 <86787475+dinesh-58@users.noreply.github.com>
2023-11-12 06:55:56 +0545

    finish today's solution
    forgot to store value in object. brain does that sometimes

--- codewars/6kyu/pell-numbers.js
+++ codewars/6kyu/pell-numbers.js
@@ -8,8 +8,9 @@ function pell(n) {
     if (n == 1) return 1n;
 
     n = BigInt(n)
-    const multiplier = numbers[n-1n] == undefined ? pell(n-1n): numbers[n-1n];
-    const adder      = numbers[n-2n] == undefined ? pell(n-2n): numbers[n-2n];
+
+    const multiplier = numbers[n-1n] || (numbers[n-1n] = pell(n-1n));
+    const adder      = numbers[n-2n] || (numbers[n-2n] = pell(n-2n));
 
     return 2n * multiplier + adder;
 }
