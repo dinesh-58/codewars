3df2ea4a08fd486ddd614a0b36d6cc6d288cfa58
dinesh-58 <86787475+dinesh-58@users.noreply.github.com>
2024-04-07 07:48:02 +0545

    improve today's solution

--- codewars/6kyu/take-a-ten-minutes-walk.js
+++ codewars/6kyu/take-a-ten-minutes-walk.js
@@ -2,19 +2,7 @@
 // date: 2024-04-07
 
 function isValidWalk(walk) {
-    if(walk.length !== 10) return false;
-    let x=0, y=0;
-    walk.forEach(dir => {
-        switch(dir) {
-            case 'n':
-                x++; break;
-            case 's':
-                x--; break;
-            case 'e':
-                y++; break;
-            case 'w':
-                y--; break;
-        } 
-    });
-    return x === 0 && y === 0;
+    if (walk.length !== 10) return false;
+    const str = walk.join();
+    return str.match(/n/g)?.length == str.match(/s/g)?.length && str.match(/e/g)?.length == str.match(/w/g)?.length;
 }
